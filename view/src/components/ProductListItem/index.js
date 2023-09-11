6cc68756f834d99e5c2c871aa71c05621a9b96df
Nika Bazadze <nikabazadze@gmail.com>
2023-09-11 18:43:25 -0400

    Created ProductListItem component

---
+++ view/src/components/ProductListItem/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Rating from '@mui/material/Rating';
+
+import styles from './ProductListItem.module.css';
+
+function ProductListItem({product}) {
+    return (
+        <div className={styles.card}>
+            <div className={styles.imgContainer}>
+                <img src={product.url} alt="Product photo"/>
+            </div>
+            <div>
+                <h3>{product.product_name}</h3>
+                <p>{product.small_description}</p>
+                <div className={styles.rating}>
+                    <Rating name="product-rating" defaultValue={product.rating} precision={0.5} readOnly />
+                    <p>{product.total_reviews} {product.total_reviews > 1 ? "reviews" : "review"}</p>
+                </div>
+                <span>${product.unit_price}</span>
+            </div>
+        </div>
+    );
+};
+
+export default ProductListItem;

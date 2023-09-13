9e60caa2f0da5701ebf49fe3d39a9aaef8eef502
Nika Bazadze <nikabazadze@gmail.com>
2023-09-13 15:07:12 -0400

    Refactored footer list items rendering

--- view/src/components/Footer/index.js
+++ view/src/components/Footer/index.js
@@ -1,74 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import styles from './Footer.module.css';
+import AlertDialog from "../AlertDialog";
 
 function Footer() {
-    function renderSupportList() {
+
+    const supportListItems = ["shipping", "contact us", "returns", "warranty", "FAQs", "privacy policy"];
+    const aboutListItems = ["about ZiPLiX", "our blog", "press & media", "careers", "our story", "ZiPLiX patents"];
+    const shopListItems = ["premium wallets", "bags", "metal wallets", "cases", "accessories", "shop all"];
+    
+    function renderList(title, items) {
         return (
-            <div>
-                <h4>support</h4>
+            <nav>
+                <h4>{title}</h4>
                 <ul>
-                    <li><a>shipping</a></li>
-                    <li><a>contact us</a></li>
-                    <li><a>returns</a></li>
-                    <li><a>warranty</a></li>
-                    <li><a>FAQs</a></li>
-                    <li><a>privacy policy</a></li>
+                    {
+                        items.map((item, index) => (
+                            <li key={index}><a>{item}</a></li>
+                        ))
+                    }
                 </ul>
-            </div>
+            </nav>
         );
     };
-
-    function renderAboutList() {
-        return (
-            <div>
-                <h4>about</h4>
-                <ul>
-                    <li><a>about ZiPLiX</a></li>
-                    <li><a>our blog</a></li>
-                    <li><a>press & media</a></li>
-                    <li><a>careers</a></li>
-                    <li><a>our story</a></li>
-                    <li><a>ZiPLiX patents</a></li>
-                </ul>
-            </div>
-        );
-    };
-
-    function renderShopList() {
-        return (
-            <div>
-                <h4>shop</h4>
-                <ul>
-                    <li><a>premium wallets</a></li>
-                    <li><a>bags</a></li>
-                    <li><a>metal wallets</a></li>
-                    <li><a>cases</a></li>
-                    <li><a>accessories</a></li>
-                    <li><a>shop all</a></li>
-                </ul>
-            </div>
-        );
-    };
-
-    function renderSubscription() {
-        return (
-            <ul>
-                <li><a>premium wallets</a></li>
-                <li><a>bags</a></li>
-                <li><a>metal wallets</a></li>
-                <li><a>cases</a></li>
-                <li><a>accessories</a></li>
-                <li><a>shop all</a></li>
-            </ul>
-        );
-    };
-
+    
     return (
         <div className={styles.footer}>
-            <div className={styles.contentContainer}>
-                {renderSupportList()}
-                {renderAboutList()}
-                {renderShopList()}
+            <div className={styles.navContainer}>
+                {renderList("support", supportListItems)}
+                {renderList("about", aboutListItems)}
+                {renderList("shop", shopListItems)}
             </div>
         </div>
     );

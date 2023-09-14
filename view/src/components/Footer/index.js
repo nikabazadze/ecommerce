efb23861d2ae3b2ae57f5976a6390d6863126e70
Nika Bazadze <nikabazadze@gmail.com>
2023-09-14 11:58:46 -0400

    Rendered Newsletter component in Footer

--- view/src/components/Footer/index.js
+++ view/src/components/Footer/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useState } from "react";
 import styles from './Footer.module.css';
 import AlertDialog from "../AlertDialog";
+import Newsletter from "../Newsletter";
 
 function Footer() {
     const [ openDialog, setOpenDialog ] = useState(false);
+
     const supportListItems = ["shipping", "contact us", "returns", "warranty", "FAQs", "privacy policy"];
     const aboutListItems = ["about ZiPLiX", "our blog", "press & media", "careers", "our story", "ZiPLiX patents"];
     const shopListItems = ["premium wallets", "bags", "metal wallets", "cases", "accessories", "shop all"];
@@ -32,6 +34,9 @@ function Footer() {
                 {renderList("support", supportListItems)}
                 {renderList("about", aboutListItems)}
                 {renderList("shop", shopListItems)}
+            </div>
+            <div className={styles.rightContent}>
+                <Newsletter />
             </div>
             {openDialog && <AlertDialog title={dialogTitle} content={dialogContent} onClose={setOpenDialog} />}
         </div>

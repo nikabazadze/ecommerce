c64916cd2b181f750f1576be6a06f139ee91a2ca
Nika Bazadze <nikabazadze@gmail.com>
2023-09-15 14:00:18 -0400

    Removed unnecessary outer div element

--- view/src/components/ProductFeatures/index.js
+++ view/src/components/ProductFeatures/index.js
@@ -3,15 +3,13 @@ import styles from './ProductFeatures.module.css';
 
 function ProductFeatures({position = "left", url, title = "", description = ""}) {
     return (
-        <div>
-            <div className={styles.feature} style={{ flexDirection: `${position === "right" && "row-reverse"}`}}>
-                <div className={styles.imgContainer}>
-                    <img src={url} alt="Feature photo"/>
-                </div>
-                <div className={styles.featureText}>
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                </div>
+        <div className={styles.feature} style={{ flexDirection: `${position === "right" && "row-reverse"}`}}>
+            <div className={styles.imgContainer}>
+                <img src={url} alt="Feature photo"/>
+            </div>
+            <div className={styles.featureText}>
+                <h2>{title}</h2>
+                <p>{description}</p>
             </div>
         </div>
     );

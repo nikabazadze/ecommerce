5c03d8a78809c508fd47af300d244d8371fb2e4f
Nika Bazadze <nikabazadze@gmail.com>
2023-09-14 23:04:26 -0400

    Refactored ProductListItem component

--- view/src/components/ProductListItem/index.js
+++ view/src/components/ProductListItem/index.js
@@ -9,14 +9,18 @@ function ProductListItem({product}) {
             <div className={styles.imgContainer}>
                 <img src={product.url} alt="Product photo"/>
             </div>
-            <div>
-                <h3>{product.product_name}</h3>
-                <p>{product.small_description}</p>
-                <div className={styles.rating}>
-                    <Rating name="product-rating" defaultValue={product.rating} precision={0.5} readOnly />
-                    <p>{product.total_reviews} {product.total_reviews > 1 ? "reviews" : "review"}</p>
+            <div className={styles.productMeta}>
+                <div>
+                    <h3>{product.product_name}</h3>
+                    <p>{product.small_description}</p>
                 </div>
-                <span>${product.unit_price}</span>
+                <div>
+                    <div className={styles.rating}>
+                        <Rating name="product-rating" defaultValue={product.rating} precision={0.5} readOnly />
+                        <p>{product.total_reviews} {product.total_reviews > 1 ? "reviews" : "review"}</p>
+                    </div>
+                    <span>${product.unit_price}</span>
+                </div>
             </div>
         </div>
     );

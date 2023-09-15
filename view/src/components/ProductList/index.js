44a1c671fa01d7971539c019f55d0ddb15076124
Nika Bazadze <nikabazadze@gmail.com>
2023-09-15 19:36:07 -0400

    Made horizontal list horizontally scrollable

--- view/src/components/ProductList/index.js
+++ view/src/components/ProductList/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Grid from '@mui/material/Grid';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 import styles from './ProductList.module.css';
 import ProductListItem from "../ProductListItem";
 import { products } from '../../data/mockData';
 
 function ProductList({position = "grid"}) {
+    function handleClick(direction) {
+        const list = document.getElementById("flexContainer");
+        const product = list.children[0];
+        const productWidth = product.offsetWidth;
+        const flexGap = 2 * 16;
+        const scrollSize = productWidth + flexGap;
+        list.scrollBy({
+            top: 0,
+            left: `${direction === "right" ? scrollSize : -scrollSize}`,
+            behavior: "smooth"
+        });
+    };
+
     return (
         <div>
             {position === "grid" ? 
@@ -21,8 +36,14 @@ function ProductList({position = "grid"}) {
                     </Grid>
                 </div>
                 :
-                <div className={styles.flexContainer}>
-                    {products.map((product) => <ProductListItem product={product}/>)}
+                <div>
+                    <div className={styles.iconContainer}>
+                        <KeyboardArrowLeftIcon  sx={{ fontSize: 28}} onClick={() => handleClick("left")} />
+                        <KeyboardArrowRightIcon sx={{ fontSize: 28}} onClick={() => handleClick("right")} />
+                    </div>
+                    <div className={styles.flexContainer} id="flexContainer">
+                        {products.map((product) => <ProductListItem product={product} />)}
+                    </div>
                 </div>
             }
         </div>

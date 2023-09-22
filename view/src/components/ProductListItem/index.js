9aa650c2cbdd4df2850df21b3da105c67fb9a8c1
Nika Bazadze <nikabazadze@gmail.com>
2023-09-22 14:31:09 -0400

    Refactored because of using real data instead of mock data

--- view/src/components/ProductListItem/index.js
+++ view/src/components/ProductListItem/index.js
@@ -7,19 +7,19 @@ function ProductListItem({ product }) {
     return (
         <div className={styles.card} >
             <div className={styles.imgContainer}>
-                <img src={product.url} alt="Product photo"/>
+                <img src={product.productVariants[0].imgUrls[0]} alt="Product photo"/>
             </div>
             <div className={styles.productMeta}>
                 <div>
-                    <h3>{product.product_name}</h3>
-                    <p>{product.small_description}</p>
+                    <h3>{product.productName}</h3>
+                    <p>{product.smallDescription}</p>
                 </div>
                 <div>
-                    <div className={styles.rating}>
-                        <Rating name="product-rating" defaultValue={product.rating} precision={0.5} readOnly />
-                        <p>{product.total_reviews} {product.total_reviews > 1 ? "reviews" : "review"}</p>
+                    <div className={styles.reviews}>
+                        <Rating name="product-rating" defaultValue={product.reviewsScore} precision={0.5} readOnly />
+                        <p>{product.reviewsQuantity} {product.reviewsQuantity > 1 ? "reviews" : "review"}</p>
                     </div>
-                    <span>${product.unit_price}</span>
+                    <span>${product.unitPrice}</span>
                 </div>
             </div>
         </div>

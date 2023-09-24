166093b8c532836be123fcf7cd6648904b397592
Nika Bazadze <nikabazadze@gmail.com>
2023-09-23 22:35:58 -0400

    Wrapped outer div in the <Link> tag

--- view/src/components/ProductListItem/index.js
+++ view/src/components/ProductListItem/index.js
@@ -1,28 +1,31 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Rating from '@mui/material/Rating';
 
 import styles from './ProductListItem.module.css';
 
 function ProductListItem({ product }) {
     return (
-        <div className={styles.card} >
-            <div className={styles.imgContainer}>
-                <img src={product.productVariants[0].imgUrls[0]} alt="Product photo"/>
-            </div>
-            <div className={styles.productMeta}>
-                <div>
-                    <h3>{product.productName}</h3>
-                    <p>{product.smallDescription}</p>
+        <Link to={`/products/${product.id}`} className={styles.link}>
+            <div className={styles.card} >
+                <div className={styles.imgContainer}>
+                    <img src={product.productVariants[0].imgUrls[0]} alt="Product photo"/>
                 </div>
-                <div>
-                    <div className={styles.reviews}>
-                        <Rating name="product-rating" defaultValue={product.reviewsScore} precision={0.5} readOnly />
-                        <p>{product.reviewsQuantity} {product.reviewsQuantity > 1 ? "reviews" : "review"}</p>
+                <div className={styles.productMeta}>
+                    <div>
+                        <h3>{product.productName}</h3>
+                        <p>{product.smallDescription}</p>
                     </div>
-                    <span>${product.unitPrice}</span>
+                    <div>
+                        <div className={styles.reviews}>
+                            <Rating name="product-rating" defaultValue={parseFloat(product.reviewsScore)} precision={0.5} readOnly />
+                            <p>{product.reviewsQuantity} {product.reviewsQuantity > 1 ? "reviews" : "review"}</p>
+                        </div>
+                        <span>${product.unitPrice}</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 

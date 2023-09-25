6d21bdb372a6ba3939fa4222105ba10b9445b40e
Nika Bazadze <nikabazadze@gmail.com>
2023-09-24 23:53:22 -0400

    Added conditional rendering along with some functions

--- view/src/pages/Product/index.js
+++ view/src/pages/Product/index.js
@@ -1,37 +1,63 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useMemo, useEffect } from "react";
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import './border.css';
 import Rating from '@mui/material/Rating';
 import styles from "./Product.module.css";
-import './border.css';
-import { products } from "../../data/mockData";
 import BasicAccordion from "../../components/Accordion";
 import ProductHighlights from "../../components/ProductHighlights";
 import ProductList from "../../components/ProductList";
+import { selectProductById, selectProducts } from "../../store/ProductsSlice";
 
 function Product() {
-    const product = products[1];
-    const [ chosenColor, setChosenColor ] = useState(product.colors[0]);
+    let { id } = useParams();
+    const products = useSelector(selectProducts);
+    const product = useSelector(selectProductById(id));
+
+    const productColors = useMemo(() => {
+        if (!product) return [];
+        return product.productVariants.map(variant => {
+            const color = {
+                name: variant.colorName,
+                code: variant.colorCode
+            };
+            return color;
+        });
+    }, [product]);
+
+    const [chosenColor, setChosenColor] = useState(null);
+
+    useEffect(() => {
+        if (productColors.length > 0) {
+            setChosenColor(productColors[0]);
+        }
+    }, [productColors]);
+
+    if (!product || !chosenColor) {
+        return <div>Loading...</div>;
+    }
+
     const accordionItems = [
         {
-            title: "Feature",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
-        },
-        {
-            title: "Specification",
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
-        },
-        {
             title: "Shipping and Delivery",
-            content: <p>Free shipping order over $65+ <br /><br />We offer regular or express shipping to most addresses worldwide. Shipping cost and delivery times are calculated at checkout. <br /><br />Note: P.O. box deliveries will automatically be sent by regular shipping.</p>
-        }
+            content: <p>Free shipping order over $75+ <br /><br />We offer regular or express shipping to most addresses worldwide. Shipping cost and delivery times are calculated at checkout. <br /><br />Note: P.O. box deliveries will automatically be sent by regular shipping.</p>
+        },
     ];
 
-    function renderColors() {
+    const getUrl = () => {
+        if (!chosenColor) return "";
+        const variant = product.productVariants.find(variant => variant.colorName === chosenColor.name);
+        return variant ? variant.imgUrls[0] : "";
+    };
+
+    const renderColors = () => {
         return (
             <ul>
                 {
-                    product.colors.map((color, index) => (
-                        <li key={index} 
+                    productColors.map((color) => (
+                        <li key={color.code} 
                             className={`${color.code === chosenColor.code && "chosenColorBorder"}`}
                             onClick={() => setChosenColor(color)}
                         ><div className={styles.color} style={{ backgroundColor: `${color.code}` }}></div></li>
@@ -41,15 +67,16 @@ function Product() {
         );
     };
 
-    function renderHighlights() {
+    const renderHighlights = () => {
         return (
             <div className={styles.highlights}>
                 {product.highlights.map((highlight, index) => (
                     <ProductHighlights 
+                        key={highlight.title}
                         position={`${index % 2 === 0 ? "right" : "left"}`}
                         url={highlight.url}
                         title={highlight.title}
-                        description={highlight.description}
+                        description={highlight.content}
                     />
                 ))}
             </div>
@@ -61,17 +88,17 @@ function Product() {
             <section className={styles.mainContainer}>
                 <div className={styles.leftSection}>
                     <div className={styles.imgContainer}>
-                        <img src={product.url} alt="Product photo" />
+                        <img src={getUrl()} alt="Product photo" />
                     </div>
                 </div>
                 <div className={styles.rightSection}>
                     <div className={styles.productMeta}>
-                        <h2>{product.product_name}</h2>
+                        <h2>{product.productName}</h2>
                         <div className={styles.rating}>
-                            <Rating name="product-rating" defaultValue={product.rating} precision={0.5} readOnly />
-                            <p>{product.total_reviews} {product.total_reviews > 1 ? "reviews" : "review"}</p>
+                            <Rating name="product-rating" defaultValue={parseFloat(product.reviewsScore)} precision={0.5} readOnly />
+                            <p>{product.reviewsQuantity} {product.reviewsQuantity > 1 ? "reviews" : "review"}</p>
                         </div>
-                        <span>${product.unit_price}</span>
+                        <span>${product.unitPrice}</span>
                     </div>
                     <div className={styles.colorsContainer}>
                         <p>{chosenColor.name}</p>
@@ -83,10 +110,10 @@ function Product() {
                     </div>
                     <div>
                         <p className={styles.shipping}>For EU and USA shipped within 1 business day</p>
-                        <p className={styles.description}>{product.description}</p>
+                        <p className={styles.description}>{product.mainDescription}</p>
                     </div>
                     <div className={styles.accordion}>
-                        <BasicAccordion items={accordionItems}/>
+                        <BasicAccordion items={accordionItems} />
                     </div>
                 </div>
             </section>
@@ -95,7 +122,7 @@ function Product() {
             </section>
             <section className={styles.suggestion}>
                 <h2>You May also Like</h2>
-                <ProductList position="horizontal"/>
+                <ProductList position="horizontal" products={products} />
             </section>
         </div>
     );

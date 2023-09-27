14cea3acc76448f7ac2244b2271074427c23f7cc
Nika Bazadze <nikabazadze@gmail.com>
2023-09-26 22:22:49 -0400

    Added sliding effect when changing image

--- view/src/components/ImageSlider/index.js
+++ view/src/components/ImageSlider/index.js
@@ -3,8 +3,17 @@ import { useState, useRef, useEffect } from "react";
 import styles from './ImageSlider.module.css';
 
 function ImageSlider({images}) {
+    const containerRef = useRef(null);
+    const mainImageRef = useRef(null);
     const imageListRef = useRef(null);
     const [ currentIndex, setCurrentIndex ] = useState(0);
+    const sliderWidth = images.length * 100;
+
+    useEffect(() => {
+        const imageWidth = containerRef.current ? containerRef.current.offsetWidth : 0;
+        const newPosition = -currentIndex * imageWidth;
+        mainImageRef.current.style.transform = `translateX(${newPosition}px)`;
+    }, [currentIndex]);
 
     const showPrevious = () => {
         const isFirstImage = currentIndex === 0;
@@ -48,10 +57,21 @@ function ImageSlider({images}) {
 
     return (
         <div className={styles.container}>
-            <div 
-                className={styles.img}
-                style={{ backgroundImage: `url(${images[currentIndex]})` }}
-            ></div>
+            <div className={styles.mainImageContainer} ref={containerRef}>
+                <div 
+                    ref={mainImageRef}
+                    className={styles.imageSlider}  
+                    style={{ width: `${sliderWidth}%` }}
+                >
+                    {images.map((image, index) => (
+                        <div 
+                            key={index} 
+                            className={styles.img}
+                            style={{ backgroundImage: `url(${image})` }}
+                        ></div>
+                    ))}
+                </div>
+            </div>
             <div className={styles.prev} onClick={showPrevious} >&#10094;</div>
             <div className={styles.next} onClick={showNext} >&#10095;</div>
             <ul ref={imageListRef}>

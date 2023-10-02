33b9312c68551a692176e1275f3804b81741f0bd
Nika Bazadze <nikabazadze@gmail.com>
2023-10-02 12:59:05 -0400

    Added addUser function

--- view/src/API/index.js
+++ view/src/API/index.js
@@ -1,6 +1,22 @@
-const API_ENDPOINT = "http://localhost:3000/api";
+const SERVER_ENDPOINT = "http://localhost:3000";
+const API_ENDPOINT = `${SERVER_ENDPOINT}/api`;
 
 export const getProducts = async () => {
     const response = await fetch(`${API_ENDPOINT}/products`);
     return response.json();
+};
+
+export const addUser = async (firstName, lastName, email, password) => {
+    const response = await fetch(`${SERVER_ENDPOINT}/signup`, {
+        method: "POST",
+        body: JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            password
+        }),
+        headers: { "Content-Type": "application/json", },
+    });
+
+    return response.status;
 };

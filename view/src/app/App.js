e6db2000f24ccb970ab8b7dfa103881de42521be
Nika Bazadze <nikabazadze@gmail.com>
2023-10-02 20:16:45 -0400

    Updated path value for route rendering Account page

--- view/src/app/App.js
+++ view/src/app/App.js
@@ -9,7 +9,7 @@ import Home from '../pages/Home';
 import Shop from '../pages/Shop';
 import Product from '../pages/Product';
 import Cart from '../pages/Cart';
-import SignUp from '../pages/SignUp';
+import Account from '../pages/Account';
 
 import { loadProducts } from '../store/ProductsSlice';
 
@@ -18,7 +18,7 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={ <Home /> } />
     <Route path='shop' element={ <Shop /> } />
     <Route path='cart' element={ <Cart /> } />
-    <Route path='signup' element={ <SignUp /> } />
+    <Route path='account/:action' element={ <Account /> } />
     <Route path='products/:id' element={ <Product /> } />
   </Route>
 ));

c27c53afcb595ef21cba4869067315b276ff3fe2
Nika Bazadze <nikabazadze@gmail.com>
2023-10-03 16:44:53 -0400

    Created user slice for storing user info

---
+++ view/src/store/UserSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        isLoggedIn: false,
+        user: null
+    },
+    reducers: {
+        setIsLoggedIn: (state, action) => {state.isLoggedIn = action.payload},
+        setUser: (state, action) => {state.user = action.payload},
+        clearUser: (state) => {state.user = null},
+    },
+});
+
+export const { setIsLoggedIn, setUser, clearUser } = userSlice.actions;
+
+export const selectIsLoggedIn = (state) => state.user.isLoggedIn;
+export const selectUser = (state) => state.user.user;
+
+export default userSlice.reducer;

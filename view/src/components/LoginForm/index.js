bda0f2b8a38f6006cc65beec12992196c2bc3662
Nika Bazadze <nikabazadze@gmail.com>
2023-10-03 16:46:45 -0400

    Saved user info after successful login

--- view/src/components/LoginForm/index.js
+++ view/src/components/LoginForm/index.js
@@ -1,26 +1,31 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import styles from './LoginForm.module.css';
 import TextField from '@mui/material/TextField';
 import { login } from "../../API";
 import AuthButton from "../AuthButton";
+import { setUser, setIsLoggedIn } from "../../store/UserSlice";
 
 function LoginForm() {
     const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         if (email, password) {
-            const status = await login(email, password);
+            const response = await login(email, password);
             
-            if (status === 200) {
-                console.log("Loged in successfuly!");
+            if (response.success) {
+                console.log(response.message);
+                dispatch(setUser(response.user));
+                dispatch(setIsLoggedIn(true));
                 navigate("/");
             } else {
-                console.log("Error! Log in wasn't processed.");
+                console.log(response.message);
             }
 
             setEmail("");

32f41a39d51d62ce2f5c7f89c1dda48bf0018c04
Nika Bazadze <nikabazadze@gmail.com>
2023-10-03 16:45:24 -0400

    Added user reducer in the store

--- view/src/store/index.js
+++ view/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './ProductsSlice';
+import userReducer from './UserSlice';
 
 export const store = configureStore({
   reducer: {
-    products: productsReducer
+    products: productsReducer,
+    user: userReducer
   },
 });

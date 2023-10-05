21d49da306ce2edc07ab60de47e0f8f01f3d9b15
Nika Bazadze <nikabazadze@gmail.com>
2023-10-05 18:21:30 -0400

    Added cart object in the user slice state

--- view/src/store/UserSlice.js
+++ view/src/store/UserSlice.js
@@ -1,21 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getUserCart } from "../API";
+
+export const loadUserCart = createAsyncThunk(
+    'user/loadUserCart',
+    getUserCart
+);
 
 export const userSlice = createSlice({
     name: "user",
     initialState: {
         isLoggedIn: false,
-        user: null
+        user: {},
+        cart: {},
+        cartIsLoading: false,
+        cartHasError: false,
     },
     reducers: {
         setIsLoggedIn: (state, action) => {state.isLoggedIn = action.payload},
         setUser: (state, action) => {state.user = action.payload},
         clearUser: (state) => {state.user = null},
     },
+    extraReducers: {
+        [loadUserCart.pending]: (state, action) => {
+            state.cartIsLoading = true;
+            state.cartHasError = false;
+        },
+        [loadUserCart.fulfilled]: (state, action) => {
+            state.cart = action.payload;
+            state.cartIsLoading = false;
+            state.cartHasError = false;
+        },
+        [loadUserCart.rejected]: (state, action) => {
+            state.cartIsLoading = false;
+            state.cartHasError = true;
+        },
+    }
 });
 
 export const { setIsLoggedIn, setUser, clearUser } = userSlice.actions;
 
 export const selectIsLoggedIn = (state) => state.user.isLoggedIn;
 export const selectUser = (state) => state.user.user;
+export const selectCart = (state) => state.user.cart;
+export const selectCartIsLoading = (state) => state.user.cartIsLoading;
+export const selectCartHasError = (state) => state.user.cartHasError;
 
 export default userSlice.reducer;

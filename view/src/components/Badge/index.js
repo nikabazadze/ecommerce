c29483ebb97f792297f522f4df5777f385bd8aea
Nika Bazadze <nikabazadze@gmail.com>
2023-10-19 12:52:07 -0400

    Created Badge component

---
+++ view/src/components/Badge/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Badge({count, size, fontSize, color}) {
+    const divStyle = {
+        fontSize: `${fontSize || "0.75rem"}`,
+        fontWeight: 500,
+        width: `${size || "1.25rem"}`,
+        height: `${size || "1.25rem"}`,
+        borderRadius: "50%",
+        backgroundColor: `${color || "#707070"}`,
+        color: "#fff",
+        position: "absolute",
+        top: 0,
+        right: 0,
+        transform: "translate(+40%, -40%)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+
+    return (
+        <div style={divStyle}>{count}</div>
+    );
+}
+
+export default Badge;

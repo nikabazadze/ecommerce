df1728ff0a41aa8c952bf1f4bd7bfce52295b74a
Nika Bazadze <nikabazadze@gmail.com>
2023-10-24 17:09:10 -0400

    Created OrdersSlice and added in the store

--- view/src/store/index.js
+++ view/src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './ProductsSlice';
 import userReducer from './UserSlice';
 import cartReducer from './CartSlice';
+import ordersReducer from './OrdersSlice';
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    orders: ordersReducer
   },
 });

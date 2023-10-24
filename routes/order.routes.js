e753ac03b77dcb801f21f14e6dc23d6adbaea778
Nika Bazadze <nikabazadze@gmail.com>
2023-10-24 17:12:30 -0400

    Slight change in the route path

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -8,7 +8,7 @@ router.param('orderId', checkOrderId);
 router.param('userId', checkUserId);
 
 router.get('/', (req, res) => req.query.status ? db.getOrdersByStatus(req, res) : db.getOrders(req, res));
-router.get('/user/:userId', (req, res) => req.query.status ? db.getUserOrdersByStatus(req, res) : db.getUserOrders(req, res));
+router.get('/users/:userId', (req, res) => req.query.status ? db.getUserOrdersByStatus(req, res) : db.getUserOrders(req, res));
 router.get('/:orderId', db.getOrderById);
 router.patch('/:orderId', db.updateOrderStatus);
 router.delete('/:orderId', db.deleteOrder);

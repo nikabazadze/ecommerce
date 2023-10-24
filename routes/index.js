4e1ff5bee6f268cdcf7aaef35b87156177c68fb1
Nika Bazadze <nikabazadze@gmail.com>
2023-10-23 23:54:22 -0400

    Separated orders router from users router

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const usersRouter = require('./user.routes');
 const productsRouter = require('./product.routes');
 const ordersRouter = require('./order.routes');
+const db = require('../controllers/checkout.controllers');
 
 // API Routes
 router.use('/users', usersRouter);
 router.use('/products', productsRouter);
+router.use('/orders', ordersRouter)
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const db = require('../controllers/user.controllers');
 const { checkUserId } = require('../middleware/user.middleware');
 const cartRouter = require('./cart.routes');
-const ordersRouter = require('./order.routes');
 
 router.param('userId', checkUserId);
 
 router.use('/:userId/cart', cartRouter);
-router.use('/:userId/orders', ordersRouter);
 
 router.get('/', db.getUsers);
 router.get('/:userId', db.getUserById);

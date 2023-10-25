be5e1ce7716c4cf68c3f12284c8418d8e8e65367
Nika Bazadze <nikabazadze@gmail.com>
2023-10-25 00:23:08 -0400

    Created ChosenProducts component

---
+++ view/src/components/ChosenProducts/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from './ChosenProducts.module.css';
+import Badge from "../Badge";
+
+function ChosenProducts({ products }) {
+    return (
+        products.map(product => (
+            <div className={styles.mainContainer} key={product.productId}>
+                <div className={styles.imgOuterContainer}>
+                    <div className={styles.imgContainer}>
+                        <img src={product.imgUrl} alt="Product image" />
+                    </div>
+                    <Badge count={product.productQuantity} />
+                </div>
+                <div className={styles.productMetaContainer}>
+                    <div>
+                        <p>{product.productName}</p>
+                        <span>{product.colorName}</span>
+                    </div>
+                    <p>${product.unitPrice}</p>
+                </div>
+            </div>
+        ))
+    );
+};
+
+export default ChosenProducts;

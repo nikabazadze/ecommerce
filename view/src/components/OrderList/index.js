7e1a8363afbffe6a2b36940358514355045ddd41
Nika Bazadze <nikabazadze@gmail.com>
2023-10-25 01:13:16 -0400

    Rendered OrderListItem component for each order

--- view/src/components/OrderList/index.js
+++ view/src/components/OrderList/index.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styles from './OrderList.module.css';
 import OrderListItem from "../OrderListItem";
+import { selectOrders } from "../../store/OrdersSlice";
 
-function OrderList() {
+function OrderList({ limit }) {
+    const orders = useSelector(selectOrders);
+
     return (
         <div className={styles.mainContainer}>
-            <OrderListItem />
+            {orders.map((order, index) => (
+                (!limit || ((index + 1) <= limit)) && <OrderListItem key={order.orderId} order={order} />
+            ))}
         </div>
     );
 }

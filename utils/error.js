67328c273c4001e2ae7443ccd1fa484da0de1545
Nika Bazadze <nikabazadze@gmail.com>
2023-10-26 23:23:02 -0400

    deleted error sender function

--- utils/error.js
+++ utils/error.js
@@ -2,8 +2,4 @@ const hasError = (obj) => {
     return Object.prototype.toString.call(obj) === "[object Error]";
 };
 
-const sendError500 = (res, err) => {
-    res.status(500).json({ message: err.message });
-};
-
-module.exports = { hasError, sendError500 };
+module.exports = { hasError };

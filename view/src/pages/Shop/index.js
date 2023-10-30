19d5666a10b58e934db04d77ed8c906fe3aa2ab5
Nika Bazadze <nikabazadze@gmail.com>
2023-10-30 00:53:21 -0400

    Removed search term clearing logic

--- view/src/pages/Shop/index.js
+++ view/src/pages/Shop/index.js
@@ -1,23 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useLocation } from 'react-router-dom';
 import styles from "./Shop.module.css";
 import ProductList from "../../components/ProductList";
-import { clearSearchTerm } from "../../store/SearchSlice";
 import { selectProducts } from "../../store/ProductsSlice";
 
 function Shop() {
     const dispatch = useDispatch();
     const location = useLocation();
     const products = useSelector(selectProducts);
-
-    useEffect(() => {
-        return () => {
-            if (location.pathname !== '/shop') {
-                dispatch(clearSearchTerm());
-            }
-        };
-    }, [dispatch, location.pathname]);
 
     return (
         <div className={styles.shopPage}>

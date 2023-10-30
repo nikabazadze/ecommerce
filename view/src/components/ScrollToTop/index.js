d7d5ea2f8c038ae6721ceafb675f096b2fc6f0c7
Nika Bazadze <nikabazadze@gmail.com>
2023-10-30 00:54:12 -0400

    Clearing search term when navigating from shop page

--- view/src/components/ScrollToTop/index.js
+++ view/src/components/ScrollToTop/index.js
@@ -1,13 +1,21 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+import { selectSearchTerm, clearSearchTerm } from '../../store/SearchSlice';
 
 function ScrollToTop() {
+  const dispatch = useDispatch();
   const location = useLocation();
   const { pathname } = location;
+  const searchTerm = useSelector(selectSearchTerm);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+
+    if (searchTerm && pathname !== '/shop') {
+      dispatch(clearSearchTerm());
+    }
+  }, [pathname, dispatch]);
 
   return null;
 };

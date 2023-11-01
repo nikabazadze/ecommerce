ec04d52deea0150b2b7ea5f635d1c48755a9285d
Nika Bazadze <nikabazadze@gmail.com>
2023-11-01 00:47:30 -0400

    Added slide-in/slide-out effect

--- view/src/components/PromoHeader/index.js
+++ view/src/components/PromoHeader/index.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import styles from './PromoHeader.module.css';
 
 function PromoHeader() {
+    const [currentPromoIndex, setCurrentPromoIndex] = useState(0);
+    const [animationClass, setAnimationClass] = useState(styles.slideIn);
+
     const promos = [
         "free shipping on order over $75+",
-        "99 day risk free trial"
+        "99 day risk free trial",
+        <div className={styles.divPromoText}>
+            <p>premium products</p>
+            <span> | </span>
+            <p>free returns</p>
+        </div>,
     ];
-    
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setAnimationClass(styles.slideOut);
+        }, 4000);
+
+        return () => clearTimeout(timeoutId);
+    }, [currentPromoIndex]);
+
+    useEffect(() => {
+        if (animationClass === styles.slideOut) {
+            const nextPromoIndex = (currentPromoIndex + 1) % promos.length;
+            setTimeout(() => {
+                setCurrentPromoIndex(nextPromoIndex);
+                setAnimationClass(styles.slideIn);
+            }, 200);
+        }
+    }, [animationClass, currentPromoIndex, promos.length]);
+
     return (
         <div className={styles.div}>
-            <span className={styles.span}>{promos[0]}</span>
-        </div>
+            <Link to="/shop" className={`${styles.link} ${animationClass}`}>{promos[currentPromoIndex]}</Link> </div>
     );
 };
 

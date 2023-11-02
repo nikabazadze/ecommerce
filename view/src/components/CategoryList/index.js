539a06020158d7c7a2a7e92956552311a6fdaefa
Nika Bazadze <nikabazadze@gmail.com>
2023-11-01 23:23:04 -0400

    Added categories mock data array

--- view/src/components/CategoryList/index.js
+++ view/src/components/CategoryList/index.js
@@ -3,9 +3,35 @@ import Grid from '@mui/material/Grid';
 
 import styles from './CategoryList.module.css';
 import CategoryListItem from "../CategoryListItem";
-import { categories } from "../../data/mockData";
 
 function CategoryList() {
+    const categories = [
+        {
+            name: "premium wallets",
+            url: "https://vulkit.com/cdn/shop/files/vulkit_premium_leather_wallet_1_d277bb04-6b60-46d2-a879-5209ccd10969_1080x.jpg?v=1686810333"
+        },
+        {
+            name: "metal wallets",
+            url: "https://m.media-amazon.com/images/I/71U0vYHASgL._AC_SX679_.jpg"
+        },
+        {
+            name: "cases",
+            url: "https://www.ekster.com/cdn/shop/files/bags_cases.png?v=1663596199&width=360"
+        },
+        {
+            name: "airtag wallets",
+            url: "https://edgessentials.com/cdn/shop/products/edge-smart-airtag-wallet-137369.webp?v=1684695957"
+        },
+        {
+            name: "accessories",
+            url: "https://www.ekster.com/cdn/shop/files/5_113c1018-7914-458c-8b50-16b2f975601d.jpg?v=1688732574&width=540"
+        },
+        {
+            name: "cases",
+            url: "https://www.ekster.com/cdn/shop/files/bags_cases.png?v=1663596199&width=360"
+        }
+    ];
+
     return (
         <div>
             <Grid container spacing={3}>

f5f8b95da056bf1fb99f9f27e497ec38cd6ece30
Nika Bazadze <nikabazadze@gmail.com>
2023-11-02 23:34:48 -0400

    Updated img sources using AWS S3 Bucket

--- view/src/components/CategoryList/index.js
+++ view/src/components/CategoryList/index.js
@@ -8,27 +8,27 @@ function CategoryList() {
     const categories = [
         {
             name: "premium wallets",
-            url: "https://vulkit.com/cdn/shop/files/vulkit_premium_leather_wallet_1_d277bb04-6b60-46d2-a879-5209ccd10969_1080x.jpg?v=1686810333"
+            url: "https://ecomm-project-ziplix.s3.amazonaws.com/homepage-images/premium-wallets.jpeg"
         },
         {
             name: "metal wallets",
-            url: "https://m.media-amazon.com/images/I/71U0vYHASgL._AC_SX679_.jpg"
+            url: "https://ecomm-project-ziplix.s3.amazonaws.com/homepage-images/metal-wallets.jpeg"
         },
         {
             name: "cases",
-            url: "https://www.ekster.com/cdn/shop/files/bags_cases.png?v=1663596199&width=360"
+            url: "https://ecomm-project-ziplix.s3.amazonaws.com/homepage-images/cases.png"
         },
         {
             name: "airtag wallets",
-            url: "https://edgessentials.com/cdn/shop/products/edge-smart-airtag-wallet-137369.webp?v=1684695957"
+            url: "https://ecomm-project-ziplix.s3.amazonaws.com/homepage-images/airtag-wallets.jpeg"
         },
         {
             name: "accessories",
-            url: "https://www.ekster.com/cdn/shop/files/5_113c1018-7914-458c-8b50-16b2f975601d.jpg?v=1688732574&width=540"
+            url: "https://ecomm-project-ziplix.s3.amazonaws.com/homepage-images/accessories.jpeg"
         },
         {
             name: "shop all",
-            url: "https://www.ekster.com/cdn/shop/files/Product_Gallery_Carrousel_5519922b-08ce-4a58-998f-1d9343275791.jpg?v=1669635845&width=900"
+            url: "https://ecomm-project-ziplix.s3.amazonaws.com/homepage-images/shop-all.jpeg"
         }
     ];
 
--- view/src/pages/Home/index.js
+++ view/src/pages/Home/index.js
@@ -21,13 +21,13 @@ function Home() {
             <section className={styles.highlights}>
                 <ProductHighlights 
                     position="left"
-                    url="https://vulkit.com/cdn/shop/files/vulkit_wallet_f0cf5b26-cd81-4af1-a59a-8edbfb546bc5_1080x.jpg?v=1679649457"
+                    url="https://ecomm-project-ziplix.s3.amazonaws.com/homepage-images/highlight1.webp"
                     title="Slim, Modern, Secure"
                     description="There is a better way to carry cards and cash. Ditch the bulk of the traditional leather bi-fold and get a slim, RFID blocking wallet that is ready to go wherever you do and stand the test of time."
                 />
                 <ProductHighlights 
                     position="right"
-                    url="https://vulkit.com/cdn/shop/files/commuter-waiting-for-subway_1080x.jpg?v=1639031973"
+                    url="https://ecomm-project-ziplix.s3.amazonaws.com/homepage-images/highlight2.jpeg"
                     title="About ZiPLiX"
                     description="We believe you should love the products you carry everyday; not only because they look great and work well, but because they actually make your life better.We believe in having less stuff, designed with more care."
                 />

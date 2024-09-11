323c42c8840e5e1c89a55fbb8a27ac6fc6092187
Nika Bazadze <nikabazadze@gmail.com>
2024-09-11 15:16:00 -0400

    Made the Header responsive

--- view/src/components/Header/index.js
+++ view/src/components/Header/index.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
 
 import styles from './Header.module.css';
 import Search from "../Search";
 import Badge from "../Badge";
+import Menu from "../Menu/Menu";
+import menuIcon from "../../assets/logos/menuIcon.svg"
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import { selectIsLoggedIn, selectUser } from "../../store/UserSlice";
 import { selectCartTotalItems } from "../../store/CartSlice";
 
 function Header() {
+    const [ showMenu, setShowMenu ] = useState(false);
     const isLoggedIn = useSelector(selectIsLoggedIn);
     const user = useSelector(selectUser);
     const cartTotalItems = useSelector(selectCartTotalItems);
@@ -30,11 +34,11 @@ function Header() {
         return (
             <ul className={styles.list}>
                 <li><Search /></li>
-                <li><Link to="account"><PersonOutlinedIcon /><span>{`Hello, ${isLoggedIn ? user.firstName : "Sign up"}`}</span></Link></li>
+                <li><Link to="account"><PersonOutlinedIcon className={styles.personIcon} /><span>{`Hello, ${isLoggedIn ? user.firstName : "Sign up"}`}</span></Link></li>
                 <li>
                     <Link to="cart">
                         <div className={styles.cartIconContainer}>
-                            <ShoppingCartOutlinedIcon sx={{fontSize: 22}} />
+                            <ShoppingCartOutlinedIcon sx={{fontSize: 22}} className={styles.cartIcon} />
                             {cartTotalItems > 0 && <Badge count={cartTotalItems} size={"0.875rem"} fontSize={"0.625rem"} translate={"30"} />}
                         </div>
                         <span>Cart</span>
@@ -45,11 +49,24 @@ function Header() {
     };
 
     return (
-        <div className={styles.container}>
-            <nav className={styles.leftNav}>{renderLeftNav()}</nav>
-            <span className={styles.logo}><Link to="/">ZiPLiX</Link></span>
-            <nav className={styles.rightNav}>{renderRightNav()}</nav>
-        </div>
+        <>
+            {/* Header */}
+            <div className={styles.outerContainer}>
+                <div className={styles.innerContainer}>
+                    <div className={styles.menuIconContainer} onClick={() => setShowMenu(!showMenu)} >
+                        <img src={menuIcon} className={styles.menuIcon} alt="Dropdown menu icon" />
+                    </div>
+                    <nav className={styles.leftNav}>{renderLeftNav()}</nav>
+                    <span className={styles.logo}><Link to="/">ZiPLiX</Link></span>
+                    <nav className={styles.rightNav}>{renderRightNav()}</nav>
+                </div>
+            </div>
+
+            {/* Menu */}
+            <div className={`${styles.menuContainer} ${showMenu ? styles.menuVisible : ''}`} >
+                <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
+            </div>
+        </>
     );
 };
 

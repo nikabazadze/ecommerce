3dfe90847d8e2715d17c1cfb017930829766efb4
Nika Bazadze <nikabazadze@gmail.com>
2024-09-27 16:05:32 -0400

    Made accordion customizable

--- view/src/components/Accordion/index.js
+++ view/src/components/Accordion/index.js
@@ -2,22 +2,34 @@ import React from 'react';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import AddIcon from '@mui/icons-material/Add';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-function BasicAccordion({items}) {
+function BasicAccordion({items, placement}) {
+    const accordionStyles = (placement === "footer") ? {
+        color: "#fff",
+        boxShadow: "none",
+        borderRadius: "0",
+        borderTop: "1px solid #fff",
+        backgroundColor: "#454545",
+    } : {};
+
     return (
         <div>
             {
-                items.map((item) => (
-                    <Accordion key={item.id}>
+                items.map((item, index) => (
+                    <Accordion key={item.id} style={accordionStyles}>
                         <AccordionSummary
-                        expandIcon={<AddIcon />}
+                        expandIcon={<ExpandMoreIcon sx={{ color: placement === "footer" ? "#fff" : "", fontSize: "22px" }} />}
                         aria-controls="panel1a-content"
                         id="panel1a-header"
+                        sx={{ 
+                            padding: placement === "footer" ? "0px" : "",
+                            borderBottom: (placement === "footer" && index === 2) ? "1px solid #fff" : "",
+                        }}
                         >
                             {item.title}
                         </AccordionSummary>
-                        <AccordionDetails>
+                        <AccordionDetails sx={{ padding: placement === "footer" ? "0 16px" : "" }} >
                             {item.content}
                         </AccordionDetails>
                     </Accordion>

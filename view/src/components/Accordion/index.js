bbf1c1313a6d04d3d0675d6babda4e4cfcaca07b
Nika Bazadze <nikabazadze@gmail.com>
2025-08-04 14:25:07 -0400

    Added conditional styling based on accordion placement

--- view/src/components/Accordion/index.js
+++ view/src/components/Accordion/index.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import styles from "./BasicAccordion.module.css"
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function BasicAccordion({items, placement}) {
-    const accordionStyles = (placement === "footer") ? {
-        color: "#fff",
-        boxShadow: "none",
-        borderRadius: "0",
-        borderTop: "1px solid #fff",
-        backgroundColor: "#454545",
-    } : {};
+    let accordionStyles = {};
+
+    if (placement === "footer") {
+        accordionStyles = {
+            color: "#fff",
+            boxShadow: "none",
+            borderRadius: "0",
+            borderTop: "1px solid #fff",
+            backgroundColor: "#454545",
+        }
+    } else if (placement === "card") {
+        accordionStyles = {
+            marginBottom: "0.625rem",
+            border: "none",
+            borderRadius: "var(--border-radius-card)",
+            boxShadow: "var(--box-shadow-card)",
+        }
+    };
+
+    const summaryStyles = (index) => {
+        return {
+            padding: (placement === "footer") ? "0px" : (placement === "card") ? "0.625rem 1.5rem" : "",
+            borderBottom: (placement === "footer" && index === 2) ? "1px solid #fff" : "",
+        };
+    };
 
     return (
         <div>
             {
                 items.map((item, index) => (
-                    <Accordion key={item.id} style={accordionStyles}>
+                    <Accordion key={item.id} style={accordionStyles} className={styles.accordion}>
                         <AccordionSummary
                         expandIcon={<ExpandMoreIcon sx={{ color: placement === "footer" ? "#fff" : "", fontSize: "22px" }} />}
                         aria-controls="panel1a-content"
                         id="panel1a-header"
-                        sx={{ 
-                            padding: placement === "footer" ? "0px" : "",
-                            borderBottom: (placement === "footer" && index === 2) ? "1px solid #fff" : "",
-                        }}
+                        className={(placement === "card") && styles.accordionSummary}
+                        sx={summaryStyles(index)}
                         >
                             {item.title}
                         </AccordionSummary>

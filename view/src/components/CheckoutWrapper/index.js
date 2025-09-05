0e0007654a758d13692bbe120e553bed60457dbe
Nika Bazadze <nikabazadze@gmail.com>
2025-09-05 16:05:18 -0400

    Fixed navigate bug

--- view/src/components/CheckoutWrapper/index.js
+++ view/src/components/CheckoutWrapper/index.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { selectCart } from "../../store/CartSlice";
 import Checkout from "../../pages/Checkout";
 import CircularProgress from '@mui/material/CircularProgress';
 
-const divStyle = {
+const center = {
     width: "100vw",
     height: "100vh",
     display: "flex",
@@ -17,18 +17,26 @@ function CheckoutWrapper() {
     const navigate = useNavigate();
     const cart = useSelector(selectCart);
 
-    useEffect(() => {
-        if (Object.keys(cart).length === 0) {
-            navigate('/cart');
-        }
-    }, [cart, navigate]);
+    // Cart is "hydrated" only once the slice exposes `cartItems`.
+    const isHydrated = cart && Object.prototype.hasOwnProperty.call(cart, "cartItems");
 
-    if (Object.keys(cart).length === 0) {
-        return  <div style={divStyle}>
-                    <CircularProgress size={60} />
-                </div>;
+    useEffect(() => {
+        if (!isHydrated) return;
+        if (cart.cartItems.length === 0) {
+        navigate('/shop', { replace: true });
+        }
+    }, [isHydrated, cart, navigate]);
+
+    // While we’re waiting for hydration (right after a refresh), show a spinner.
+    if (!isHydrated) {
+        return (
+        <div style={center}>
+            <CircularProgress size={60} />
+        </div>
+        );
     }
 
+    // If hydrated and non-empty, allow access to Checkout.
     return <Checkout />;
 }
 
